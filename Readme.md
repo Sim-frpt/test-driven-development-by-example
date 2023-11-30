4a7df7ac038a7bda6479efad1402035e17002303
Simon Fraipont <simon.fraipont@intotheweb.be>
2023-11-30 16:17:34 +0100

    end chapter 3

--- Readme.md
+++ Readme.md
@@ -1,5 +1,9 @@
 - $5 + 10CHF = $10 if rate is 2:1
 - ~~$5 \* 2 = $10~~
 - Make "amount" private
-- **Dollar side-effects?**
+- ~~Dollar side-effects?~~
 - Money rounding?
+- ~~Equals() ?~~
+- HashCode()
+- Equal null
+- Equal object
--- src/money.test.ts
+++ src/money.test.ts
@@ -10,3 +10,8 @@ test("multiplication", () => {
   five.times(3);
   expect(product.amount).toBe(15);
 });
+
+test("equality", () => {
+  expect(new Dollar(5).equals(new Dollar(5))).toEqual(true);
+  expect(new Dollar(5).equals(new Dollar(6))).toEqual(false);
+});
