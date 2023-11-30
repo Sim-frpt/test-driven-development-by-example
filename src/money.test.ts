ea0a1399ce16675070addb85d0a32cb1f0240ed6
Simon Fraipont <simon.fraipont@intotheweb.be>
2023-11-30 16:28:56 +0100

    finish chapter 4: privacy

--- src/money.test.ts
+++ src/money.test.ts
@@ -2,13 +2,9 @@ import { Dollar } from "./models";
 
 test("multiplication", () => {
   const five: Dollar = new Dollar(5);
-  let product: Dollar = five.times(2);
 
-  expect(product.amount).toBe(10);
-
-  product = five.times(3);
-  five.times(3);
-  expect(product.amount).toBe(15);
+  expect(five.times(2)).toEqual(new Dollar(10));
+  expect(five.times(3)).toEqual(new Dollar(15));
 });
 
 test("equality", () => {
